1620dd2fa2a62766f9d0baebe92a780ba63760dc
erik78se <erik.lonroth@gmail.com>
2021-12-14 15:43:26 +0100

    * templates, requirements, metadata, etc.

--- ipfs-cluster/src/charm.py
+++ ipfs-cluster/src/charm.py
@@ -5,14 +5,15 @@
 import logging
 import os
 import shutil
-
-
+from pathlib import Path
+import requests
 from ops.charm import CharmBase
 from ops.framework import StoredState
 from ops.main import main
 from ops.model import ActiveStatus, WaitingStatus, MaintenanceStatus
-import subprocess as sp
+import subprocess
 import sys
+import jinja2
 
 import utils
 
@@ -24,9 +25,13 @@ EMOJI_GREEN_DOT = "\U0001F7E2"
 EMOJI_RED_DOT = "\U0001F534"
 EMOJI_PACKAGE = "\U0001F4E6"
 
+IPFS_HOME = Path('/opt/ipfs/')
+IPFS_CTL = Path('/opt/ipfs/ipfs-cluster-ctl/')
+IPFS_SERVICE = Path('/opt/ipfs/ipfs-cluster-service/')
+IPFS = Path('/opt/ipfs/go-ipfs/')
 
 class IPFSClusterCharm(CharmBase):
-    """Charm the hello service with all core hooks."""
+    """IPFS cluster with all core hooks."""
 
     _stored = StoredState()
 
@@ -44,37 +49,33 @@ class IPFSClusterCharm(CharmBase):
         self.framework.observe(self.on.collect_metrics, self._on_collect_metrics)
         self.framework.observe(self.on.upgrade_charm, self._on_upgrade_charm)
         # peer
-        self.framework.observe(self.on.peer_relation_joined, self._on_peer_relation_joined)
-        self.framework.observe(self.on.peer_relation_departed, self._on_peer_relation_departed)
-        self.framework.observe(self.on.peer_relation_changed, self._on_peer_relation_changed)
+        self.framework.observe(self.on.cluster_relation_joined, self._on_cluster_relation_joined)
+        self.framework.observe(self.on.cluster_relation_departed, self._on_cluster_relation_departed)
+        self.framework.observe(self.on.cluster_relation_changed, self._on_cluster_relation_changed)
 
 
-        self._stored.set_default(sources_uri=self.config["sources-uri"],
+        self._stored.set_default(service_sources_uri=self.config["service-sources-uri"],
+                                 ctl_sources_uri=self.config["ctl-sources-uri"],
                                  restart_on_reconfig=self.config["restart-on-reconfig"])
 
 
     def _on_install(self, event):
         """
-        Install your software here plus any dependencies, utilities and everything you need to run
-        your service over time.
-
-        Optionally take care of installing resources attached with the charm deployment.
-
-        In this charm, we install a package and a service unit file which will use hello.
-
-        This hook is ran after the storage-filesystem-attached hook - only once in the entire lifetime of the unit.
+        Install ipfs-cluster
         """
         logger.debug(EMOJI_CORE_HOOK_EVENT + sys._getframe().f_code.co_name)
 
-        logger.info(f"Installing from snap ipfs {EMOJI_PACKAGE}")
-        # os.system(f"snap install ipfs --channel={self._stored.snap_channel}")
-        utils.fetch_and_extract_sources(self._stored.sources_uri)
-        
-        # Install unit file for hello (one-shot service)
-        shutil.copyfile('templates/etc/systemd/system/ipfs-cluster.service', '/etc/systemd/system/ipfs-cluster.service')
+        logger.info(f"Installing ctl from source uri {EMOJI_PACKAGE}")
+        os.system(f"mkdir -p {IPFS_HOME}")
+        utils.fetch_and_extract_sources(self._stored.ctl_sources_uri, IPFS_HOME)
 
-        # (re)config hello.
-        self._reconfig_ipfs(restart=False)
+        logger.info(f"Installing service from source uri {EMOJI_PACKAGE}")     
+        utils.fetch_and_extract_sources(self._stored.service_sources_uri, IPFS_HOME)
+        
+        shutil.copyfile('templates/etc/systemd/system/ipfs-cluster.service', '/etc/systemd/system/ipfs-cluster.service')
+        
+        # (re)config
+        self._reconfig_ipfs_cluster(restart=False)
 
 
     def _on_config_changed(self, event):
@@ -89,10 +90,14 @@ class IPFSClusterCharm(CharmBase):
         """
         logger.debug(EMOJI_CORE_HOOK_EVENT + sys._getframe().f_code.co_name)
 
-        if self.config["sources-uri"] != self._stored.sources_uri:
-            self._stored.sources_uri = self.config["sources-uri"]
+        if self.config["ctl-sources-uri"] != self._stored.ctl_sources_uri:
+            self._stored.ctl_sources_uri = self.config["ctl-sources-uri"]
             self._reconfig_ipfs_cluster(restart=self.config["restart-on-reconfig"])
 
+        if self.config["service-sources-uri"] != self._stored.service_sources_uri:
+            self._stored.service_sources_uri = self.config["service-sources-uri"]
+            self._reconfig_ipfs_cluster(restart=self.config["restart-on-reconfig"])
+            
         self._on_update_status(event)
 
     def _on_start(self, event):
@@ -100,23 +105,24 @@ class IPFSClusterCharm(CharmBase):
             Start your service here, possibly defer (wait) until conditions are OK.
         """
         logger.debug(EMOJI_CORE_HOOK_EVENT + sys._getframe().f_code.co_name)
-        logger.info(f"{EMOJI_GREEN_DOT} Starting the ipfs-daemon service...")
-        os.system('systemctl start ipfs-daemon.service')
+        logger.info(f"{EMOJI_GREEN_DOT} Starting ipfs-cluster.service")
+        os.system('systemctl start ipfs-cluster.service')
 
         # Calling update_status gives quick feedback when deploying starts up.
         self._on_update_status(event)
+        self.unit.set_workload_version(utils.getIpfsClusterVersion())
+
 
     def _on_leader_elected(self, event):
         """
             This is only run on the unit which is selected by juju as leader.
-            We are not implementing anything here. See the "leadership" charm for an example.
         """
         logger.debug(EMOJI_CORE_HOOK_EVENT + sys._getframe().f_code.co_name)
-
+        os.system('sudo -u ubuntu /opt/ipfs/ipfs-cluster-service/ipfs-cluster-service init --force')
+        
     def _on_leader_settings_changed(self, event):
         """
             This is only run on the unit which is selected by juju as leader.
-            We are not implementing anything here. See the "leadership" charm for an example.
         """
         logger.debug(EMOJI_CORE_HOOK_EVENT + sys._getframe().f_code.co_name)
 
@@ -128,24 +134,30 @@ class IPFSClusterCharm(CharmBase):
         """
         logger.debug(EMOJI_CORE_HOOK_EVENT + sys._getframe().f_code.co_name)
 
-        if not os.system('systemctl is-active ipfs-daemon.service') == 0:
-            logger.info("ipfs-daemon service is not running.")
+        if not os.system('systemctl is-active ipfs-cluster.service') == 0:
+            logger.info("ipfs-cluster service is not running.")
             self.unit.status = MaintenanceStatus("Inactive.")
         else:
             logger.info(f"ipfs-daemon service is running.")
             self.unit.status = ActiveStatus("Running.")
 
+        if self.model.unit.is_leader():
+                self.unit.set_workload_version(utils.getIpfsClusterVersion())
+
+            
     def _on_upgrade_charm(self, event):
         logger.debug(EMOJI_CORE_HOOK_EVENT + sys._getframe().f_code.co_name)
 
+        shutil.copyfile('templates/etc/systemd/system/ipfs-cluster.service', '/etc/systemd/system/ipfs-cluster.service')
+        
     def _on_stop(self, event):
         """
         Bring down your service, possibly defer until all systems are good to go similar to start hook.
         """
         logger.debug(EMOJI_CORE_HOOK_EVENT + sys._getframe().f_code.co_name)
 
-        logger.info(f"{EMOJI_RED_DOT} Stopping the ipfs-daemon service...")
-#        os.system('systemctl stop ipfs-daemon.service')
+        logger.info(f"{EMOJI_RED_DOT} Stopping the ipfs-cluster service...")
+        os.system('systemctl stop ipfs-cluster.service')
 
 
     def _on_remove(self, event):
@@ -169,36 +181,54 @@ class IPFSClusterCharm(CharmBase):
         logger.debug(EMOJI_CORE_HOOK_EVENT + sys._getframe().f_code.co_name)
 
 
-    def _on_peer_relation_joined(self, event):
+    def _on_cluster_relation_joined(self, event):
         logger.debug(EMOJI_CORE_HOOK_EVENT + sys._getframe().f_code.co_name)
         pass
 
-    def _on_peer_relation_departed(self, event):
+    def _on_cluster_relation_departed(self, event):
         logger.debug(EMOJI_CORE_HOOK_EVENT + sys._getframe().f_code.co_name)
         pass
 
-    def _on_peer_relation_changed(self, event):
+    def _on_cluster_relation_changed(self, event):
         logger.debug(EMOJI_CORE_HOOK_EVENT + sys._getframe().f_code.co_name)
         pass
 
+    def _ipfs_init(self):
+        """
+        ./ipfs-cluster-service init
+        2021-12-05T16:16:47.604Z	INFO	config	config/config.go:481	Saving configuration
+        configuration written to /home/ubuntu/.ipfs-cluster/service.json.
+        2021-12-05T16:16:47.607Z	INFO	config	config/identity.go:73	Saving identity
+        new identity written to /home/ubuntu/.ipfs-cluster/identity.json
+        new empty peerstore written to /home/ubuntu/.ipfs-cluster/peerstore.
+        """
+        os.chdir(f"{IPFS_SERVICE}")
+        os.system('./ipfs-cluster-service init')
+        
     def _reconfig_ipfs_cluster(self, restart=False):
         """
-        Reconfigures the startup parameters of hello.service by modifying the /etc/default/hello file.
+        Reconfigures the startup parameters of hello.service by modifying the /etc/default/ipfs-cluster file.
         Reloads systemd daemons.
 
         Optionally, restart the service.
         """
-        logger.info(f"{EMOJI_MESSAGE} Configuring ipfs-daemon snap-channel: {self._stored.snap_channel}")
-#        self._stored.snap_channel = self.config["snap-channel"]
-#        os.system(f"snap refresh ipfs --channel={self._stored.snap_channel}")
-
-        with open('/etc/default/ipfs-cluster', 'w') as f:
-            f.write(f"CUSTOM_ARGS=\\'cluster\\'")
+        logger.info(f"{EMOJI_MESSAGE} Re-Configuring")
+        
+        template = jinja2.Environment(
+            loader=jinja2.FileSystemLoader(
+                os.path.join(self.charm_dir, 'templates/etc/default/'))).get_template('ipfs-cluster.j2') 
+        target = Path('/etc/default/ipfs-cluster')
+        ctx = {'peername': 'peeeeeername',
+               'secret': 'seeeeecret',
+               'cluster_id': 'cluster_iiiiiiid'}
+        target.write_text(template.render(ctx))
+        
         os.system('systemctl daemon-reload')
 
         if restart:
-            logger.info(f"{EMOJI_GREEN_DOT} Restarting hello.")
-#            os.system('systemctl restart ipfs-daemon.service')
+            logger.info(f"{EMOJI_GREEN_DOT} Restarting ipfs-cluster.")
+            os.system('systemctl restart ipfs-cluster.service')
 
+            
 if __name__ == "__main__":
     main(IPFSClusterCharm)
--- ipfs-cluster/src/utils.py
+++ ipfs-cluster/src/utils.py
@@ -1,15 +1,26 @@
 import subprocess
-import requestes
+import requests
+import tarfile
+from pathlib import Path
+import io
 
-def fetch_and_extract_sources(tarfile_url):
+def fetch_and_extract_sources(tarfile_url, dstpath):
     """
     Fetch and Install sources from internet
     """
     try:
         response = requests.get(tarfile_url, allow_redirects=True, stream=True)
-        dst = Path('/opt/ipfs-cluster/')
-        with tarfile.open(fileobj=io.BytesIO(response.content), mode='r:bz2') as tfile:
+        dst = Path(dstpath)
+        with tarfile.open(fileobj=io.BytesIO(response.content), mode='r:gz') as tfile:
             tfile.extractall(path=dst)
     except subprocess.CalledProcessError as e:
         print(e)
         sys.exit(-1)
+
+def getIpfsClusterVersion():
+    resp = subprocess.check_output(['/opt/ipfs/ipfs-cluster-service/ipfs-cluster-service','--version']).decode()
+    return resp.rstrip().rpartition(' ')[-1] # Get the version
+
+def getIpfsVersion():
+    resp = subprocess.check_output(['ipfs','--version']).decode()
+    return resp.rstrip().rpartition(' ')[-1] # Get the version
